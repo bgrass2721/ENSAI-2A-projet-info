acd00dddea22c4082f20b786c40881b35976084c
id2657 <hugo.platel@eleve.ensai.fr>
2025-10-30 10:11:43 +0000

    dao

--- src/dao/dao.py
+++ src/dao/dao.py
@@ -42,6 +42,7 @@ def add_attack(self, attack: AbstractAttack) -> bool:
 """
 
 from business_object.chanson import Chanson
+from business_object.playlist import Playlist
 from dao.db_connection import DBConnection
 
 
@@ -52,26 +53,25 @@ class DAO:
                 cursor.execute("""
                     CREATE TABLE IF NOT EXISTS PLAYLIST (
                     id_playlist SERIAL PRIMARY KEY,
-                    nom VARCHAR(255) NOT NULL,
-                    emb_nom FLOAT8[], 
+                    nom VARCHAR(255) NOT NULL, 
                     date_creation DATE DEFAULT CURRENT_DATE
                     );
                     CREATE TABLE IF NOT EXISTS CHANSON (
                     embed_paroles FLOAT8[] PRIMARY KEY,
                     titre VARCHAR(255) NOT NULL,
                     artiste VARCHAR(255) NOT NULL,
-                    annee INT,
+                    annee INT
                     );
                     CREATE TABLE IF NOT EXISTS CATALOGUE (
                     id_playlist INT NOT NULL,
-                    embed_paroles INT NOT NULL,
+                    embed_paroles FLOAT8[] NOT NULL,
                     PRIMARY KEY (id_playlist, embed_paroles),
                     FOREIGN KEY (id_playlist) REFERENCES PLAYLIST(id_playlist) ON DELETE CASCADE,
                     FOREIGN KEY (embed_paroles) REFERENCES CHANSON(embed_paroles) ON DELETE CASCADE
                     );
                     """)
 
-    def add_chanson(chanson: Chanson):
+    def add_chanson(self, chanson: Chanson):
         embed_paroles = chanson.paroles.vecteur
         titre = chanson.titre
         artiste = chanson.artiste
@@ -81,7 +81,7 @@ class DAO:
                 cursor.execute(
                     """
                     INSERT INTO CHANSON (embed_paroles, titre, artiste, annee)
-                    VALUES (%(embed_paroles)s, %(titre)s, %(artiste)s, %(annee)s)
+                    VALUES (%(embed_paroles)s, %(titre)s, %(artiste)s, %(annee)s);
                     """,
                     {
                         "embed_paroles": embed_paroles,
@@ -90,3 +90,27 @@ class DAO:
                         "annee": annee,
                     },
                 )
+
+    def add_playlist(self, playlist: Playlist):
+        nom = playlist.nom
+        chansons = playlist.chansons
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    INSERT INTO PLAYLIST (nom)
+                    VALUES (%s)
+                    RETURNING id_playlist;
+                    """,
+                    (nom,),
+                )
+                id_playlist = cursor.fetchone()
+                for chanson in chansons:
+                    embed_paroles = chanson.paroles.vecteur
+                    cursor.execute(
+                        """
+                        INSERT INTO CATALOGUE (id_playlist, embed_paroles)
+                        VALUES (%s, %s);
+                        """,
+                        (id_playlist, embed_paroles),
+                    )
