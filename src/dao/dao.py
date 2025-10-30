939520f262f6c451e333deb36dabf6d3f4873638
id2657 <hugo.platel@eleve.ensai.fr>
2025-10-30 18:52:19 +0000

    dao

--- src/dao/dao.py
+++ src/dao/dao.py
@@ -33,7 +33,7 @@ class DAO:
                     """)
                 connection.commit()
 
-    def add_chanson(self, chanson: Chanson):
+    def add_chanson(self, chanson: Chanson) -> None:
         """
         Ajoute une chanson à la table CHANSON de la BD
         """
@@ -59,7 +59,7 @@ class DAO:
                 )
                 connection.commit()
 
-    def add_playlist(self, playlist: Playlist):
+    def add_playlist(self, playlist: Playlist) -> None:
         """
         Ajoute une playlist à la table PLAYLIST de la BD et remplit la table CATALOGUE de la BD
         """
@@ -152,3 +152,70 @@ class DAO:
                     titre, artiste, annee = res
                     return Chanson(titre, artiste, annee, paroles)
         return None
+
+    def del_chanson_via_embed_paroles(self, embed_paroles: list[float]) -> None:
+        """
+        Supprime une chanson de la table CHANSON, soit via l'embedding de ses paroles
+        """
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    DELETE FROM CHANSON
+                    WHERE embed_paroles = %s;
+                    """,
+                    (embed_paroles,),
+                )
+            connection.commit()
+
+    def del_chanson_via_titre_artiste(self, titre: str, artiste: str) -> None:
+        """
+        Supprime une chanson de la table CHANSON, soit via l'embedding de ses paroles
+        """
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    DELETE FROM CHANSON
+                    WHERE titre = %s
+                    AND artiste = %s;
+                    """,
+                    (titre, artiste),
+                )
+            connection.commit()
+
+    def del_playlist_via_nom(self, nom: str) -> None:
+        """
+        Supprime une playlist de la table PLAYLIST via son nom.
+        Les lignes associées dans CATALOGUE sont supprimées
+        automatiquement grace au ON DELETE CASCADE
+        """
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    DELETE FROM PLAYLIST
+                    WHERE nom = %s;
+                    """,
+                    (nom,),
+                )
+            connection.commit()
+
+    def del_data_table(self, nom_table: str | None = None) -> None:
+        """
+        Vide complètement les tables passées en argument.
+        Si aucune table n'est spécifiée, vide toutes les tables sont vidées
+        """
+        # Ordre logique de suppression pour respecter les contraintes FK
+        ordre = ["CATALOGUE", "PLAYLIST", "CHANSON"]
+        if isinstance(nom_table, str) and nom_table.upper() in ordre:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(f"DELETE FROM {nom_table.upper()};")
+            connection.commit()
+        if isinstance(nom_table, None):
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    for nom_table in ordre:
+                        cursor.execute(f"DELETE FROM {nom_table};")
+            connection.commit()
