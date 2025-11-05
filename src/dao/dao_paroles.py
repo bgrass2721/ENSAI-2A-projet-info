4aa10adcd72f83634b453b789b97f543a320bce2
id2657 <hugo.platel@eleve.ensai.fr>
2025-11-05 11:47:38 +0000

    dao

--- src/dao/dao_paroles.py
+++ src/dao/dao_paroles.py
@@ -6,17 +6,19 @@ from dao.db_connection import DBConnection
 class DAO_paroles(DAO):
     def get_paroles(self) -> list[Paroles]:
         """
-        Liste l'objet Paroles de toutes les chansons enregistrées dans la BD
+        Liste les Paroles de toutes les chansons enregistrées dans la BD
         """
         list_Paroles = []
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
-                cursor.execute("SELECT embed_paroles, str_paroles FROM CHANSON;")
-                resultat = (
-                    cursor.fetchall()
-                )  # [(embed_paroles, str_paroles), (embed_paroles, str_paroles),]
-                for tup in resultat:
-                    vecteur, content = tup
-                    paroles = Paroles(content=content, vecteur=vecteur)
-                    list_Paroles.append(paroles)
-        return list_Paroles
+                cursor.execute("""
+                    SELECT embed_paroles, str_paroles 
+                    FROM CHANSON;
+                    """)  # [(embed_paroles, str_paroles), (embed_paroles, str_paroles), ...]
+                res = cursor.fetchall()
+                if res:
+                    for embed_paroles, str_paroles in res:
+                        paroles = Paroles(content=str_paroles, vecteur=embed_paroles)
+                        list_Paroles.append(paroles)
+                    return list_Paroles
+        return None
